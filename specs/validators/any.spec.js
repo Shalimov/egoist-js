336fb3ebe66ef1073fab0a301126e4b45822c80c
Shalimov <Shalimich@gmail.com>
2018-04-27 12:39:12 +0300

    Add new validators for strings and any modules

--- specs/validators/any.spec.js
+++ specs/validators/any.spec.js
@@ -1,10 +1,10 @@
 import should from 'should'
 
-import { required } from '../../lib/validators/any'
+import { required, allow } from '../../lib/validators/any'
 import ERROR_KEYS from '../../lib/defaults/keys'
 
 describe('Validators Module ANY Spec', () => {
-	it('should return one item tuple with key if value has an error, otherwise return null', () => {
+	it('should return one item tuple with key if value has an error, otherwise null', () => {
 		should(required('hello')).be.Null()
 		should(required('')).be.Null()
 		should(required(0)).be.Null()
@@ -13,5 +13,24 @@ describe('Validators Module ANY Spec', () => {
 
 		required(null).should.be.eql([ERROR_KEYS.ANY.REQUIRED])
 		required(undefined).should.be.eql([ERROR_KEYS.ANY.REQUIRED])
+	})
+
+	it('should return tuple with key and allowed values if value has an error, otherwise null', () => {
+		const helloWorldAllowed = allow(['hello', 'world'])
+		const oneToFiveAllowed = allow([1, 2, 3, 4, 5])
+
+		should(helloWorldAllowed('hello')).be.Null()
+		should(helloWorldAllowed('world')).be.Null()
+		should(helloWorldAllowed('not hello')).be.eql([ERROR_KEYS.ANY.ALLOW, ['hello', 'world']])
+		should(helloWorldAllowed('not world')).be.eql([ERROR_KEYS.ANY.ALLOW, ['hello', 'world']])
+
+		should(oneToFiveAllowed(1)).be.Null()
+		should(oneToFiveAllowed(2)).be.Null()
+		should(oneToFiveAllowed(3)).be.Null()
+		should(oneToFiveAllowed(4)).be.Null()
+		should(oneToFiveAllowed(5)).be.Null()
+
+		should(oneToFiveAllowed(0)).be.eql([ERROR_KEYS.ANY.ALLOW, [1, 2, 3, 4, 5]])
+		should(oneToFiveAllowed(6)).be.eql([ERROR_KEYS.ANY.ALLOW, [1, 2, 3, 4, 5]])
 	})
 })
--- specs/validators/string.spec.js
+++ specs/validators/string.spec.js
@@ -1,10 +1,18 @@
 import should from 'should'
 
-import { isString, isNotEmpty, isEmail, match } from '../../lib/validators/string'
+import {
+	isString,
+	isNotEmpty,
+	isEmail,
+	match,
+	isAlphanum,
+	isDigits,
+	isISODate,
+} from '../../lib/validators/string'
 import ERROR_KEYS from '../../lib/defaults/keys'
 
 describe('Validators Module String Spec', () => {
-	it('should return one item tuple with key if value is not a string, otherwise return null', () => {
+	it('should return one item tuple with key if value is not a string, otherwise null', () => {
 		isString(NaN).should.be.eql([ERROR_KEYS.STRING.TYPE])
 		isString(0).should.be.eql([ERROR_KEYS.STRING.TYPE])
 		isString({}).should.be.eql([ERROR_KEYS.STRING.TYPE])
@@ -18,7 +26,7 @@ describe('Validators Module String Spec', () => {
 		should(isString('')).be.Null()
 	})
 
-	it('should return one item tuple with key if value has an error, otherwise return null', () => {
+	it('should return one item tuple with key if value has an error, otherwise null', () => {
 		isNotEmpty('').should.be.eql([ERROR_KEYS.STRING.EMPTY])
 
 		should(isNotEmpty(undefined)).be.Null()
@@ -26,7 +34,7 @@ describe('Validators Module String Spec', () => {
 		should(isNotEmpty('Test')).be.Null()
 	})
 
-	it('should return a tuple with key and regex pattern if value has an error, otherwise return null', () => {
+	it('should return a tuple with key and regex pattern if value has an error, otherwise null', () => {
 		const pattern = /^[a-zA-Z\s]+$/
 		const username = match(pattern)
 
@@ -38,8 +46,7 @@ describe('Validators Module String Spec', () => {
 		username('Test 123').should.be.eql([ERROR_KEYS.STRING.MATCH, pattern])
 	})
 
-	it('should return a tuple with key if value is not an email addres, otherwise return null', () => {
-
+	it('should return a tuple with key if value is an invalid email address, otherwise null', () => {
 		should(isEmail('hello@world.com')).be.Null()
 		should(isEmail('he@wo.by')).be.Null()
 		should(isEmail(undefined)).be.Null()
@@ -49,5 +56,50 @@ describe('Validators Module String Spec', () => {
 		isEmail('shalimich@mail.').should.be.eql([ERROR_KEYS.STRING.EMAIL])
 		isEmail('@mail.com').should.be.eql([ERROR_KEYS.STRING.EMAIL])
 		isEmail('www.com.com').should.be.eql([ERROR_KEYS.STRING.EMAIL])
+	})
+
+	it('should return a tuple with key if value is not a digit line, otherwise null', () => {
+		should(isDigits('0')).be.Null()
+		should(isDigits('10')).be.Null()
+		should(isDigits('999')).be.Null()
+		should(isDigits('9999')).be.Null()
+		should(isDigits('10101')).be.Null()
+		should(isDigits(undefined)).be.Null()
+		should(isDigits(null)).be.Null()
+
+		isDigits('01@10').should.be.eql([ERROR_KEYS.STRING.DIGITS])
+		isDigits('shalimich').should.be.eql([ERROR_KEYS.STRING.DIGITS])
+		isDigits('0@mail0').should.be.eql([ERROR_KEYS.STRING.DIGITS])
+		isDigits('101010-10201').should.be.eql([ERROR_KEYS.STRING.DIGITS])
+	})
+
+	// Here we have a blind spots but believe in regex (shallow examination)
+	it('should return a tuple with key if value is non-alphanumeric, otherwise null', () => {
+		should(isAlphanum('0')).be.Null()
+		should(isAlphanum('10')).be.Null()
+		should(isAlphanum('a999b')).be.Null()
+		should(isAlphanum('a999B')).be.Null()
+		should(isAlphanum('a9CDE9B')).be.Null()
+		should(isAlphanum('010ABCDE')).be.Null()
+		should(isAlphanum('10101')).be.Null()
+		should(isAlphanum(undefined)).be.Null()
+		should(isAlphanum(null)).be.Null()
+
+		isAlphanum('01@10').should.be.eql([ERROR_KEYS.STRING.ALPHANUM])
+		isAlphanum('0@mail0').should.be.eql([ERROR_KEYS.STRING.ALPHANUM])
+		isAlphanum('10-1010-10201').should.be.eql([ERROR_KEYS.STRING.ALPHANUM])
+		isAlphanum('10.1010.10201').should.be.eql([ERROR_KEYS.STRING.ALPHANUM])
+	})
+
+	it('should return a tuple with key if value is non-alphanumeric, otherwise null', () => {
+		should(isISODate(new Date().toISOString())).be.Null()
+		should(isISODate(undefined)).be.Null()
+		should(isISODate(null)).be.Null()
+
+		should(isISODate(new Date().toString())).be.eql([ERROR_KEYS.STRING.ISO_DATE])
+		isISODate('01@10').should.be.eql([ERROR_KEYS.STRING.ISO_DATE])
+		isISODate('0@mail0').should.be.eql([ERROR_KEYS.STRING.ISO_DATE])
+		isISODate('10-1010-10201').should.be.eql([ERROR_KEYS.STRING.ISO_DATE])
+		isISODate('10.1010.10201').should.be.eql([ERROR_KEYS.STRING.ISO_DATE])
 	})
 })
